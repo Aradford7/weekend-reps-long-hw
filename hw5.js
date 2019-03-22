cd68a97c6082329061473553c0a607e07708c34e
Ashley Radford <aradford7@outlook.com>
2019-03-22 15:22:42 -0700

    Commit 1 Q&A answered

---
+++ hw5.js
@@ -0,0 +1,24 @@
+console.log('Test 1..2...3....')
+
+//Q + A
+//How do we assign a value to a variable?
+        //To assign a variable use var(global affect out of scope), let (affects in the scope),
+        //const stays const in the scope it is assigned cannot be changed.
+
+        //How do we change the value of a variable?
+        // by declare it var something; let something; const something;
+
+//How do we assign an existing variable to a new variable?
+        //by define it like var something = {}; or let something = {}; const something = {}
+
+//Remind me, what are declare, assign, and define?
+        //declaration //var carName// variable has no value
+        //assigning a value to car like number color ect
+        //defining a variable needs an assignment operators like =
+
+//What is pseudocoding and why should you do it?
+        //helps plan out code and able to to give a outline of the problem most important!
+
+//What percentage of time should be spent thinking about how you're 
+//going to solve a problem vs actually typing in code to solve it?
+        //programming is 75-90% planning and 10-25% of actual typing!
