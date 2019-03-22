452dfc60c8ae82d5aef63f45f820f315d33e5de0
Ashley Radford <aradford7@outlook.com>
2019-03-22 15:48:25 -0700

    Commit - 2 Strings answered

--- hw5.js
+++ hw5.js
@@ -1,6 +1,6 @@
 console.log('Test 1..2...3....')
 
-//Q + A
+//A]Q + A
 //How do we assign a value to a variable?
         //To assign a variable use var(global affect out of scope), let (affects in the scope),
         //const stays const in the scope it is assigned cannot be changed.
@@ -22,3 +22,28 @@ console.log('Test 1..2...3....')
 //What percentage of time should be spent thinking about how you're 
 //going to solve a problem vs actually typing in code to solve it?
         //programming is 75-90% planning and 10-25% of actual typing!
+
+//B] Strings
+//1) Create a variable called firstVariable
+    var firstVariable 
+//2) Assign it the value of the string "Hello World"
+    var firstVariable = "Hello World";
+//3)Change the value of this variable to some number
+    var firstVariable = 10;
+//4) Store the value of the firstVariable in a newVariable called
+//secondVariable
+    var secondVariable = firstVariable;
+//5) Change the value of secondVariable to any string
+    var secondVariable = "Goodbye World";
+//6) What is the value of the firstVariable?
+    console.log(firstVariable); //Answer is 10. like it step 3
+
+//2] Create a variable called yourName and set it equal to your
+//name as a string. Then write an expression that takes the string
+//"Hello my name is " and the variable your name and a new string to return them
+    var yourName = "Slim Shady";
+    //var saying = "Hello, my name is";
+    
+    var saying = "Hello, my name is"+ ' ' + yourName;
+    console.log(saying);
+       
