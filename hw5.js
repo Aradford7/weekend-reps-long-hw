6fb3df9daf60a2483b47d210993d56e050868e70
Ashley Radford <aradford7@outlook.com>
2019-03-24 19:06:19 -0700

    Commit Array Talk about it Answered

--- hw5.js
+++ hw5.js
@@ -196,13 +196,13 @@ console.log(bank_account);
 //If we list all the natural numbers below 10 that are multiples of 3 or 5, 
 //we get 3, 5, 6 and 9. The sum of these multiples is 23.
 //Find the sum of all the multiples of 3 or 5 below 1000. 
-
+/*
 for (
     var sum = 0, i = 1;
     i <= 1000;
     !(i % 3 && i % 5) && (sum += i), i++
 );
-console.log(sum); 
+console.log(sum); */
 //Answer 233168  
 //Commit Multiples of 3 and 5 Answered
 
