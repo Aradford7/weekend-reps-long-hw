1b0d5cd12021d7afbf17755db735dca268183e3b
Ashley Radford <aradford7@outlook.com>
2019-03-24 22:13:25 -0700

    Commit transmogrify answered

--- hw5.js
+++ hw5.js
@@ -467,7 +467,7 @@ console.log(getMultipleLengths(["hello", "what", "is", "up", "dude"]));
     }
 console.log(maxOfThree(6, 9, 1));
 */
-
+/*
 //printLongestWord
 //Write a function printLongestWord that accepts a single argument, an array of strings. 
 //The method should return the longest word in the array. In case of a tie, the method should 
@@ -493,3 +493,20 @@ console.log(maxOfThree(6, 9, 1));
     }
 
 console.log(printLongestWord(["BoJack", "Princess", "Diane", "a", "Max", "Peanutbutter", "big", "Todd"]));
+*/
+
+
+//transmogrify
+//Write a Javascript function called transmogrify. This function should accept three arguments, 
+//which you can assume will be numbers. Your function should return the "transmogrified" result.
+//The transmogrified result of three numbers is the product of the first two numbers, 
+//raised to the power of the third number.
+//For example, the transmogrified result of 5, 3, and 2 is (5 times 3) to the power of 2 is 225.
+    function transmogrify(num1, num2, pow){
+        console.log(Math.pow((num1 * num2),pow));
+    }
+    console.log(transmogrify(5, 3, 2));
+
+
+
+
